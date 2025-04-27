7d6097c2f7327d0c10f7ac2591d51d886549e8dd
Koren Ma'avari <koren130@gmail.com>
2025-04-27 22:25:38 +0300

    Unitest Hardware DHT11TemperatureAndHumiditySensor

---
+++ Unit Tests/DHT11TemperatureAndHumiditySensorModule.cpp
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+// Pin where the DHT11 is connected
+#define DHTPIN 4       // GPIO 4
+#define DHTTYPE DHT11  // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);  // DHT11 needs about 2 seconds between readings
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature(); // Celsius by default
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+}
